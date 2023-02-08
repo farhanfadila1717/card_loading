280d6a05a352f73bf4e154781d43201bd578a236
Farhan Mac <farhan.fadila1717@gmail.com>
2023-02-08 07:41:48 +0700

    Theme add default theme

--- lib/src/card_loading_theme.dart
+++ lib/src/card_loading_theme.dart
@@ -1,5 +1,22 @@
 part of 'card_loading.dart';
 
+/// {@template card_loading_theme}
+///
+/// The `CardLoadingTheme` class defines the visual appearance of a loading card.
+///
+/// This class takes in two `Color` parameters, `colorOne` and `colorTwo`,
+/// which are used to set the background color of the card during its loading state.
+///
+/// Example usage:
+///
+/// ```dart
+/// CardLoadingTheme(
+///   colorOne: Colors.lightBlue,
+///   colorTwo: Colors.blue,
+/// )
+/// ```
+///
+/// {@endtemplate}
 class CardLoadingTheme {
   /// at the beginning this will be the [Color] for the background [CardLoading]
   /// default Color(0xFFE5E5E5)
@@ -9,8 +26,14 @@ class CardLoadingTheme {
   /// default Color(0xFFF0F0F0)
   final Color colorTwo;
 
+  /// {@macro card_loading_theme}
   const CardLoadingTheme({
-    this.colorOne = const Color(0xFFE5E5E5),
-    this.colorTwo = const Color(0xFFF0F0F0),
+    required this.colorOne,
+    required this.colorTwo,
   });
+
+  static const CardLoadingTheme defaultTheme = CardLoadingTheme(
+    colorOne: const Color(0xFFE5E5E5),
+    colorTwo: const Color(0xFFF0F0F0),
+  );
 }
